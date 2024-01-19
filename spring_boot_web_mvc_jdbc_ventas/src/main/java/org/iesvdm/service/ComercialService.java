512d386c60051ebe87be37e74034879c4d13cd67
aaracarc <124054983+aaracarc@users.noreply.github.com>
2024-01-19 21:47:58 +0100

    Add files via upload

---
+++ spring_boot_web_mvc_jdbc_ventas/src/main/java/org/iesvdm/service/ComercialService.java
@@ -0,0 +1,55 @@
+package org.iesvdm.service;
+
+import org.iesvdm.dao.ComercialDAO;
+import org.iesvdm.modelo.Cliente;
+import org.iesvdm.modelo.Comercial;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ComercialService {
+
+    private ComercialDAO comercialDAO;
+
+    //Se utiliza inyección automática por constructor del framework Spring.
+    //Por tanto, se puede omitir la anotación Autowired
+    //@Autowired
+    public ComercialService(ComercialDAO comercialDAO) {
+        this.comercialDAO = comercialDAO;
+    }
+
+    public List<Comercial> listAll() {
+
+        return comercialDAO.getAll();
+
+    }
+
+    public Comercial one(Integer id) {
+        Optional<Comercial> optCor = comercialDAO.find(id);
+        if (optCor.isPresent())
+            return optCor.get();
+        else
+            return null;
+    }
+
+    public void newComercial(Comercial comercial) {
+
+        comercialDAO.create(comercial);
+
+    }
+
+    public void replaceComercial(Comercial comercial) {
+
+        comercialDAO.update(comercial);
+
+    }
+
+    public void deleteComercial(int id) {
+
+        comercialDAO.delete(id);
+
+    }
+
+}
